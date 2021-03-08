de0ee441b5353d12f3ec5a800d55d7a1248cab43
vvictorious <vvictorious30@gmail.com>
2021-03-08 12:42:32 -0700

    gettin used to ts so I am starting from scratch to solidify what I am learning

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,11 @@
-import React, { useState } from 'react';
-import TodoList from './TodoList'
-import AddTodoForm from './AddTodoForm'
-
-const initialTodos: Array<Todo> = [
-  {
-    text: 'Practice ts just a little bit longer',
-    complete: false   
-  }
-]
+import React from 'react';
 
 const App: React.FC = () => {
 
-  const [todos, setTodos] = useState(initialTodos)
-
-  const toggleTodo: ToggleTodo = selectedTodo => {
-    const newTodos = todos.map(todo => {
-      if (todo === selectedTodo) {
-        return {
-          ...todo,
-          complete: !todo.complete
-        }
-      } 
-      return todo
-    })
-    setTodos(newTodos)
-  }
-
-  const addTodo: AddTodo = newTodo => {
-    setTodos([
-      ...todos,
-      {
-        text: newTodo,
-        complete: false
-      }
-    ])
-  }
 
   return (
     <div>
-      <TodoList todos={todos} toggleTodo={toggleTodo} />
-      <AddTodoForm addTodo={addTodo} />
+      <h1>hi</h1>
     </div>
   );
 }
